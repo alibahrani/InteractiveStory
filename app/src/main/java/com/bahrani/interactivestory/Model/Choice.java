62c8c73bedc20624b7ab003f60ddd144dfae59b5
Ali Al-Bahrani <alibahrani@mac.com>
2016-09-29 00:09:35 -0400

    Initial commit

---
+++ app/src/main/java/com/bahrani/interactivestory/Model/Choice.java
@@ -0,0 +1,34 @@
+package com.bahrani.interactivestory.Model;
+
+/**
+ * Created by aliba on 9/28/2016.
+ */
+
+public class Choice {
+
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+
+
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
